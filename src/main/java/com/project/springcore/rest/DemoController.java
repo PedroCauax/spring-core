b0ff76a05335afbd1d037cb4be41a2cdde3cb345
Pedro Cauã <pcaua6372@gmail.com>
2026-03-10 11:01:54 -0300

    Bean Scopes

--- src/main/java/com/project/springcore/rest/DemoController.java
+++ src/main/java/com/project/springcore/rest/DemoController.java
@@ -6,21 +6,35 @@ import com.project.springcore.common.Coach;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 public class DemoController {
 
     private Coach myCoach;
+    private Coach anotherCoach;
 
-    public DemoController(@Qualifier("baseballCoach") Coach theCoach) {
+    public DemoController(
+        @Qualifier("cricketCoach") Coach theCoach, 
+        @Qualifier("cricketCoach") Coach theAnotherCoach){ 
         System.out.println("in constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
+        
     }
+
+
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+    
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing myCoach and anotherCoach: " + (myCoach == anotherCoach);
     }
     
 }
